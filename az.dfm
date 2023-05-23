4b5ecc99d53ddcb496251c360298e7751a2ebf84
Azimjon23 <134445334+Azimjon23@users.noreply.github.com>
2023-05-24 01:39:34 +0500

    Add files via upload

---
+++ az.dfm
@@ -0,0 +1,34 @@
+object Form5: TForm5
+  Left = 0
+  Top = 0
+  Caption = 'Form5'
+  ClientHeight = 231
+  ClientWidth = 505
+  Color = clMoneyGreen
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object start: TButton
+    Left = 48
+    Top = 112
+    Width = 175
+    Height = 41
+    Caption = 'start'
+    Style = bsCommandLink
+    TabOrder = 0
+    OnClick = startClick
+  end
+  object ListBox1: TListBox
+    Left = 253
+    Top = 88
+    Width = 121
+    Height = 97
+    ItemHeight = 13
+    TabOrder = 1
+  end
+end
---
+++ az.h
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------
+
+#ifndef azH
+#define azH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm5 : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *start;
+	TListBox *ListBox1;
+	void __fastcall startClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm5(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm5 *Form5;
+//---------------------------------------------------------------------------
+#endif
